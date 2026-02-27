d7fd2e8736614891df972d6856aaa86685a65312
dongchenjie <umaydie@icloud.com>
2026-02-27 14:29:12 +0800

    cleanup: remove built js and node_modules from repo

--- src/scenes/BootScene.js
+++
@@ -1,9 +0,0 @@
-import Phaser from "phaser";
-export default class BootScene extends Phaser.Scene {
-    constructor() {
-        super({ key: "Boot" });
-    }
-    create() {
-        this.scene.start("CharacterSelect");
-    }
-}
